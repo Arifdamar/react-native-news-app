a969a2dbe7af36a9747df082cdc31f16d6663752
Arif Damar <arif660damar@gmail.com>
2021-12-13 20:03:30 +0300

    add news source

--- API/Context.js
+++ API/Context.js
@@ -1,26 +1,53 @@
 import axios from "axios";
-import React, { useState, createContext, useEffect } from 'react';
-import { getNewsAPI } from "./api";
+import React, { createContext, useEffect, useState } from "react";
+import { getNewsAPI, getSourceAPI } from "./api";
 
 export const NewsContext = createContext();
 
 const Context = ({ children }) => {
     const [news, setNews] = useState([]);
     const [category, setCategory] = useState("general");
+    const [source, setSource] = useState();
     const [index, setIndex] = useState(1);
 
-    const fetchNews = async () => {
-        const { data } = await axios.get(getNewsAPI(category));
-
+    const fetchNews = async (reset = category) => {
+        const { data } = await axios.get(getNewsAPI(reset));
         setNews(data);
         setIndex(1);
-    }
+    };
+
+    const fetchNewsfromSource = async () => {
+        try {
+            const { data } = await axios.get(getSourceAPI(source));
+            setNews(data);
+            setIndex(1);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
         fetchNews();
     }, [category]);
 
-    return <NewsContext.Provider value={{ news, index, setIndex, fetchNews }}>{children}</NewsContext.Provider>
-}
+    useEffect(() => {
+        fetchNewsfromSource();
+    }, [source]);
+
+    return (
+        <NewsContext.Provider
+            value={{
+                news,
+                setCategory,
+                index,
+                setIndex,
+                setSource,
+                fetchNews,
+            }}
+        >
+            {children}
+        </NewsContext.Provider>
+    );
+};
 
 export default Context;
